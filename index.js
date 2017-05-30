fb0631853349628f585dddb3387b8617fa44677e
Ernesto Rojas <ernesto20145@gmail.com>
2017-05-30 01:08:02 -0400

    Commit init.

---
+++ index.js
@@ -0,0 +1,21 @@
+const Quark = require('proton-quark')
+
+module.exports = class Error extends Quark {
+
+  constructor(proton) {
+    super(proton)
+  }
+
+  configure() {
+    // TODO Develop behavior
+  }
+
+  validate() {
+    // TODO Develop behavior
+  }
+
+  initialize() {
+    // TODO Develop behavior
+  }
+
+}
